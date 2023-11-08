8ae9bd2f6bc6e7fc4d999dcc3415300f77f8c424
root <root@DESKTOP-VU6TFLN>
2023-11-08 16:29:33 +0700

    pbo

---
+++ uts_pbo/src/database/migrations/2023_10_27_020123_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('Nama')->nullable();
+            $table->string('Nim')->nullable();
+            $table->string('Fakultas')->nullable();
+            $table->string('Prodi')->nullable();    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
