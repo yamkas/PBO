81b965910d204ead5a87d961f4667dc36e94bec0
root <root@DESKTOP-VU6TFLN>
2023-11-16 22:28:29 +0700

    pbo

---
+++ pbo/src/app/Filament/Resources/UTSResource/Pages/CreateUTS.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\UTSResource\Pages;
+
+use App\Filament\Resources\UTSResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUTS extends CreateRecord
+{
+    protected static string $resource = UTSResource::class;
+}
